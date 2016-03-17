ff03d2d84c788ce8de5fd94933bbcab028e7614c
DagInge <dihelgoy@gmail.com>
2016-03-17 12:29:45 +0100

    Board class finalized

--- src/Board.java
+++ src/Board.java
@@ -3,29 +3,83 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Board {
     BoardElement[][] board = new BoardElement[Values.FLATLAND_BOARD_SIZE][Values.FLATLAND_BOARD_SIZE];
+    Pair<Integer, Integer> agentCoordinate;
+    int direction; // 0: North, 1: East, 2: South, 3: West
+    int[] colDirection = {0, 1, 0, -1};
+    int[] rowDirection = {-1, 0, 1, 0};
+    static ArrayList<Pair<Integer, Integer>> coordinates = new ArrayList<>();
+    static Random random = new Random();
 
     public Board(){
         for(int y = 0; y < Values.FLATLAND_BOARD_SIZE; y++){
             for(int x = 0; x < Values.FLATLAND_BOARD_SIZE; x++){
                 board[y][x] = BoardElement.EMPTY;
+                coordinates.add(new Pair<>(x, y));
             }
         }
         initBoard();
     }
 
     private void initBoard() {
-        board[0][3] = BoardElement.FOOD;
-        board[1][2] = BoardElement.FOOD;
-        board[1][3] = BoardElement.FOOD;
-        board[1][4] = BoardElement.FOOD;
-        board[3][2] = BoardElement.AGENT;
-        board[3][3] = BoardElement.POISON;
-        board[4][5] = BoardElement.POISON;
-        board[5][6] = BoardElement.POISON;
+        fillFood();
+        fillPoison();
+        fillAgent();
+        getSensors();
+    }
 
+    public void moveForeward(){
+        agentCoordinate.setElement1((agentCoordinate.getElement1()+colDirection[direction] + Values.FLATLAND_BOARD_SIZE) % Values.FLATLAND_BOARD_SIZE);
+        agentCoordinate.setElement2((agentCoordinate.getElement2() + rowDirection[direction] + Values.FLATLAND_BOARD_SIZE) % Values.FLATLAND_BOARD_SIZE);
+    }
+    public void moveRight(){
+        direction++;
+        direction = (direction + 4) % 4;
+        moveForeward();
+    }
+    public void moveLeft(){
+        direction--;
+        direction = (direction + 4) % 4;
+        moveForeward();
+    }
+
+    private void fillFood() {
+        int foodCount = (Values.FLATLAND_BOARD_SIZE*Values.FLATLAND_BOARD_SIZE) / 3;
+        while (foodCount > 0) {
+            fillElement(BoardElement.FOOD);
+            foodCount--;
+        }
+    }
+
+    private void fillPoison() {
+        int poisonCount = (Values.FLATLAND_BOARD_SIZE*Values.FLATLAND_BOARD_SIZE*2) / 9;
+        while(poisonCount > 0) {
+            fillElement(BoardElement.POISON);
+            poisonCount--;
+        }
+    }
+
+    private void fillAgent() {
+        fillElement(BoardElement.AGENT);
+    }
+
+    private void fillElement(BoardElement element) {
+        Pair<Integer, Integer> coordinate = coordinates.remove(random.nextInt(coordinates.size()));
+        agentCoordinate = coordinate;
+        board[coordinate.getElement2()][coordinate.getElement1()] = element;
+    }
+
+    public ArrayList<BoardElement> getSensors(){
+        ArrayList<BoardElement> sensors = new ArrayList<>();
+        for(int i = -1; i < 2; i++){
+            int sensorDirection = (direction + i + 4) % 4;
+            sensors.add(board[(agentCoordinate.getElement2() + rowDirection[sensorDirection] + Values.FLATLAND_BOARD_SIZE) % Values.FLATLAND_BOARD_SIZE][(agentCoordinate.getElement1() + colDirection[sensorDirection] + Values.FLATLAND_BOARD_SIZE) % Values.FLATLAND_BOARD_SIZE]);
+        }
+        return sensors;
     }
 
     public GridPane drawGrid() {
